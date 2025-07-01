234f74c026ae440f74147e524298fab6379b41e8
drinkstrawberrysmoothie <strawberrynmilk251202@gmail.com>
2025-07-01 13:33:05 +0700

    added files and modified files to the commit message.
    cleared some boxes in index.html and added a new file index2.html

--- script.js
+++ script.js
@@ -1,110 +1,11 @@
-
-// get player choices
-function playerChoices() {
-  let playerInput =  prompt(`Rock Paper Scissors? | Whose win 5 rounds first will be the winner.`);
-  let plChoice = (playerInput === null) ? "You cancelled the game."
-  : playerInput.trim().toLowerCase();
-  return plChoice;
-  }
-
-// get PC choices
-function pcChoices() {
-  function getRandomNumFromZeroTo2(max) {
-    let aNum0To2 = Math.round(Math.random() * max);
-    return aNum0To2;
-  }
-
-  function choices(randomNum) {
-    let theChoice = (randomNum == 0) ? "rock" :
-    (randomNum == 1) ? "paper": "scissors";
-    return theChoice;
-    } 
-  return choices(getRandomNumFromZeroTo2(2));
+function createParagraph() {
+  const para = document.createElement("p");
+  para.textContent = "You clicked the button!";
+  document.body.appendChild(para);
 }
 
+const buttons = document.querySelectorAll("button");
 
-// the thing to play the game
-function playGame() {
-let humanPoints = 0;
-let pcPoints = 0;
-function playRound(humanChoice, computerChoice) {
-
-  if (humanChoice === "rock") {
-    if (computerChoice === "scissors") {
-      humanPoints += 1;
-      console.log(`You win! | Human's choice: ${humanChoice} | Computer's choice: ${computerChoice}.`);
-      console.log(`Human's score: ${humanPoints} | Computer's score: ${pcPoints}.`);
-    } else if (computerChoice === humanChoice) {
-      humanPoints += 0;
-      pcPoints += 0;
-      console.log(`A draw! | Human's choice: ${humanChoice} | Computer's choice: ${computerChoice}.`);
-      console.log(`Human's score: ${humanPoints} | Computer's score: ${pcPoints}.`);
-    } else if (computerChoice === "paper") {
-      pcPoints += 1;
-      console.log(`You lose! | Human's choice: ${humanChoice} | Computer's choice: ${computerChoice}.`);
-      console.log(`Human's score: ${humanPoints} | Computer's score: ${pcPoints}`);
-    }
-  } 
-  if (humanChoice === "paper") {
-    if (computerChoice === "rock") {
-      humanPoints += 1;
-      console.log(`You win! | Human's choice: ${humanChoice} | Computer's choice: ${computerChoice}.`);
-      console.log(`Human's score: ${humanPoints} | Computer's score: ${pcPoints}.`);
-    } else if (computerChoice === humanChoice) {
-      humanPoints += 0;
-      pcPoints += 0;
-      console.log(`A draw! | Human's choice: ${humanChoice} | Computer's choice: ${computerChoice}.`);
-      console.log(`Human's score: ${humanPoints} | Computer's score: ${pcPoints}.`);
-    } else if (computerChoice === "scissors") {
-      pcPoints += 1;
-      console.log(`You lose! | Human's choice: ${humanChoice} | Computer's choice: ${computerChoice}.`);
-      console.log(`Human's score: ${humanPoints} | Computer's score: ${pcPoints}`);
-    }
-  }
-  if (humanChoice === "scissors") {
-    if (computerChoice === "paper") {
-      humanPoints += 1;
-      console.log(`You win! | Human's choice: ${humanChoice} | Computer's choice: ${computerChoice}.`);
-      console.log(`Human's score: ${humanPoints} | Computer's score: ${pcPoints}.`);
-    } else if (computerChoice === humanChoice) {
-      humanPoints += 0;
-      pcPoints += 0;
-      console.log(`A draw! | Human's choice: ${humanChoice} | Computer's choice: ${computerChoice}.`);
-      console.log(`Human's score: ${humanPoints} | Computer's score: ${pcPoints}`);
-    } else if (computerChoice === "rock") {
-      pcPoints += 1;
-      console.log(`You lose! | Human's choice: ${humanChoice} | Computer's choice: ${computerChoice}.`);
-      console.log(`Human's score: ${humanPoints} | Computer's score: ${pcPoints}`);
-    }
-  }
-  if (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
-    if (humanChoice === "You cancelled the game.") {
-      humanPoints += 7;
-      console.log("You cancelled the game.");
-    } else if (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
-      console.log("Please type 'Rock' or 'Paper' or 'Scissors' to play.");
-    }
-  }
+for (const button of buttons) {
+  button.addEventListener("click", createParagraph);
 }
-  do {
-    const humanChoices = playerChoices();
-    const computerChoices = pcChoices()
-    playRound(humanChoices, computerChoices);
-
-      switch (humanPoints) {
-        case 5:
-          console.log(`You are the winner!`);
-            break;
-        }
-    
-      switch(pcPoints) {
-          case 5:
-            console.log(`Computer is the winner!`);
-            break;
-        }
-    } while (humanPoints < 5 && pcPoints < 5);
-}
-
-playGame();
-
-
