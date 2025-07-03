3f5a075b8caaa2098ce13a969b72badd7c1dedee
drinkstrawberrysmoothie <strawberrynmilk251202@gmail.com>
2025-07-03 14:43:58 +0700

    add 2 functions and 2 var to script.js
    I have added two functions for the player and computer choices,
    and two variables to store the choices.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("This is a script file loaded from the 'script.js' file.");
+let computerChoice = () => {
+  let choices = Math.floor(Math.random() * 3);
+  switch (choices) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissors";
+  }
+}
+console.log(computerChoice());
+
+let getHumanChoice = () => {
+  let choices = prompt("Enter your choice: Rock, Paper, or Scissors");
+  choices = choices.charAt(0).toUpperCase() + choices.slice(1).toLowerCase();
+  if (choices !== "Rock" && choices !== "Paper" && choices !== "Scissors") {
+    alert("Invalid choice! Please try again.");
+    return getHumanChoice();
+  }
+  return choices;
+}
+console.log(getHumanChoice());
+
+let computerScore = 0;
+let humanScore = 0;
